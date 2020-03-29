033a5209fc187c76b79c2b25744ee76f5120282a
FrancescaPerin <36476749+FrancescaPerin@users.noreply.github.com>
2020-03-29 16:00:04 +0200

    Added a second mechanism to generate the ballot dependent on number of projects

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import numpy as np
 import sys
 import argparse
+import random as rn
 
 
 @dataclass
@@ -33,6 +34,42 @@ def generate_agents(number_of_agents: int = 100, value_dimensions: int = 3) -> l
     return list_of_agents
 
 
+def generate_projects(num_projects:int=10, value_dimensions:int=3) -> list:
+    projects_pref=list()
+
+    for i in range (num_projects):
+        pref=rn.randrange(0,value_dimensions+1, 1)
+        project=np.zeros(value_dimensions)
+        project[:pref]= 1
+        project_f=np.random.permutation(project)
+
+
+        projects_pref.append(project_f)
+
+    return projects_pref
+
+def generate_profile_preference(voter_set, budget: int=100, num_projects:int=10)->np.ndarray:
+
+    profile = np.ndarray((len(voter_set),num_projects))
+    for voter in voter_set:
+
+        projects_pref=generate_projects(num_projects,len(voter_set[1].value_preferences))
+        projects_cost=[voter_set[1].value_preferences*p for p in projects_pref]
+        
+        #print("------PROJECT COST----------")
+        #print (projects_cost)
+
+        sum_proj_pref=[sum(p) for p in projects_cost]
+        norm_proj_pref= sum_proj_pref/sum(sum_proj_pref)
+
+        #print(sum_proj_pref)
+        #print("Then we normalize:")
+        #print(norm_proj_pref)
+
+        profile[voter.id] = norm_proj_pref*budget
+
+    return profile
+
 def generate_profile(voter_set,budget: int=100) -> np.ndarray:
     #TODO
     """
@@ -59,10 +96,14 @@ def calculate_vote(profile) -> list:
     return NotImplemented
 
 
-def generate_and_simulate(number_of_agents, value_dimensions, budget):
+def generate_and_simulate(number_of_agents, value_dimensions, budget, num_projects):
     voter_set = generate_agents(number_of_agents, value_dimensions)
     profile = generate_profile(voter_set=voter_set,budget=budget)
+    profile_pref = generate_profile_preference(voter_set=voter_set,budget=budget, num_projects=num_projects)
+    print("---Prajakta's profile----")
     print(profile)
+    print("---Francesca's profile----")
+    print(profile_pref)
     calculate_vote(profile=profile)
 
 if __name__ == "__main__":
@@ -75,7 +116,10 @@ if __name__ == "__main__":
     parser.add_argument('budget',type=int,default=100,
                              help='Total budget that needs to be distributed over projects')
 
+    parser.add_argument('num_projects',type=int,default=10,
+                             help='Total number of projects over which the budget needs to be distributed')
+
     args = parser.parse_args()
 
-    generate_and_simulate(args.n_of_agents, args.n_of_dimensions,args.budget)
+    generate_and_simulate(args.n_of_agents, args.n_of_dimensions,args.budget, args.num_projects)
 
